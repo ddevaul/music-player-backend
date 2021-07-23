111f6ce07cb87946dba17b7e32e78c2797c3b123
Desi <desi.devaul@gmail.com>
2021-07-23 18:00:38 -0400

    added artists to the admin page

--- spotify_api/admin.py
+++ spotify_api/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from .models import (Playlist, Song, OrderedPlaylistSong)
+from .models import (Playlist, Song, OrderedPlaylistSong, Artist)
 
 # Register your models here.
 admin.site.register(Playlist)
 admin.site.register(Song)
 admin.site.register(OrderedPlaylistSong)
-
-
-
+admin.site.register(Artist)
